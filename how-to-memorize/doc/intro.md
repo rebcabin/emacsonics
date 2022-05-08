70e3ec0c3437d8cac2b3298336f2be210c7b1fb5
rebcabin <rebcabin@users.noreply.github.com>
2022-05-08 12:05:09 -0700

    how to memorize

---
+++ how-to-memorize/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to how-to-memorize
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ how-to-memorize/test/how_to_memorize/core_test.clj
@@ -0,0 +1,7 @@
+(ns how-to-memorize.core-test
+  (:require [clojure.test :refer :all]
+            [how-to-memorize.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
